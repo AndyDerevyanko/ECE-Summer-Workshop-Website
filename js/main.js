9b38e80270b440a8ed11a81f2848567722b2a96a
andy <andyderevyanko@gmail.com>
2026-07-17 00:27:20 -0400

    Changed labels to say tentative. will be editable in console

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-// landing page countdown
+// landing page countdown, disabled while the start date is tbd
+// (hero shows the "to be announced" box instead, see index.html.
+//  uncomment this and the countdown markup when dates are locked)
 
-//placeholder for now
-function setCountdown() {
-  var ids = ["cd-d", "cd-h", "cd-m", "cd-s"];
-  for (var i = 0; i < ids.length; i++) {
-    var el = document.getElementById(ids[i]);
-    if (el) el.textContent = "99";
-  }
-}
-
-document.addEventListener("DOMContentLoaded", setCountdown);
+// function setCountdown() {
+//   var ids = ["cd-d", "cd-h", "cd-m", "cd-s"];
+//   for (var i = 0; i < ids.length; i++) {
+//     var el = document.getElementById(ids[i]);
+//     if (el) el.textContent = "99";
+//   }
+// }
+//
+// document.addEventListener("DOMContentLoaded", setCountdown);

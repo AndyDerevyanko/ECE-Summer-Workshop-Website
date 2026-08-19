c3da5d1dfce4bdeabff8bdff5484dfcaf9b0fcab
andy <andyderevyanko@gmail.com>
2026-08-19 15:25:18 -0400

    made template passwords public for testing

---
+++ app/seed_accounts.py
@@ -0,0 +1,20 @@
+"""login accounts used to seed the users table on first run. gitignored,
+never committed, same reason the old js/keys.js wasn't committed either."""
+
+SEED_STUDENTS = {
+    "student01": "r4fx-08nn", "student02": "sp2t-z9yj", "student03": "m55a-s897",
+    "student04": "fl68-tyo7", "student05": "uwzk-wq65", "student06": "5768-4dli",
+    "student07": "lkbx-si0h", "student08": "iln2-qsh4", "student09": "cww8-b4ar",
+    "student10": "k7co-begs", "student11": "nobq-toga", "student12": "z48s-pjv9",
+    "student13": "2dc0-bnfo", "student14": "lg2t-c4kr", "student15": "3v9i-ehnv",
+    "student16": "disc-h6od", "student17": "7a8c-tlf8", "student18": "glkk-zynu",
+    "student19": "s9fc-mlnw", "student20": "rlwi-bvia", "student21": "zh3w-vdo8",
+    "student22": "0tdr-kewu", "student23": "taan-dry8", "student24": "zxuc-ulhb",
+    "student25": "hn6w-k6zw",
+    "s": "a"
+}
+SEED_TAS = {
+    "ta-1": "1pzz-j4h5",
+    "ta-2": "n66w-rbg3",
+    "t": "a"
+}
